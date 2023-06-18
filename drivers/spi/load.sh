96308857ea3dd6f7dc4d44717c55b50b6ee7423e
Comma Device <device@comma.ai>
2023-06-17 17:46:58 -0700

    spidev

--- drivers/spi/load.sh
+++ drivers/spi/load.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-make
+make -j8
 
 sudo su -c "echo spi0.0 > /sys/bus/spi/drivers/spidev/unbind" || true
 
 sudo rmmod pandaspi || true
-sudo insmod pandaspi.ko
+sudo rmmod spidev_panda || true
+#sudo insmod pandaspi.ko
+sudo insmod spidev_panda.ko
 
 sudo lsmod
 echo "loaded"
