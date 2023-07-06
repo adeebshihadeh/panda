3f3fceeb60e0c1b1ec06ce072701fa630771e577
Comma Device <device@comma.ai>
2023-07-06 14:05:45 -0700

    cleanup

--- drivers/spi/pull-src.sh
+++ drivers/spi/pull-src.sh
@@ -4,7 +4,7 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
+rm -f spidev.c
 wget https://raw.githubusercontent.com/commaai/agnos-kernel-sdm845/master/drivers/spi/spidev.c
 
-cp spidev.c spidev_panda.c
-
+patch -o spidev_panda.c spidev.c -i patch
